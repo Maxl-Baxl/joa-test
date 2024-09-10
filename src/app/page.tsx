db0914379b5d129a791f1a6dbf8003c58ad9c36d
Maxl-Baxl <147283865+Maxl-Baxl@users.noreply.github.com>
2024-09-10 07:20:29 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,35 +28,7 @@ export default function Home() {
         </div>
       </nav>
       <h1 className="text-lg mt-5 ml-5 mb-3">
-        Das hier ist die inoffizielle MGM-Webseite
-      </h1>
-      <Link
-        className="text-blue-600 m-5 underline"
-        href="stundenplan-oberstufe"
-      >
-        Hier geht es zum Stundenplan-Generator für die Oberstufe
-      </Link>
-      <p className="m-5">
-        Info: Diese Webseite wird von Max und Timur (Q12) erstellt. Max kümmert
-        sich mehr um die Funktionen, Timur mehr ums Design. Später werden hier
-        viele Features sein, die auf der offiziellen MGM-Seite unserer Meinung
-        nach einfach fehlen. Seid gespannt, da kommt bald viel cooles dazu.
-      </p>
-      <p className="m-5">
-        Disclaimer: Mit der Webseite haben wir erst am 09.09.2024 begonnen. Da
-        so eine Webseite nicht gerade eben mal erstellt wird, ist sie noch sehr
-        limitiert, das Design ist beispielsweise komplett provisorisch. Ich
-        (Max) wollte jedoch unbedingt ein Stundenplan-Erstell-Feature haben,
-        also hab ich mich am selben Tag noch hingesetzt und von morgens bis
-        abends durchprogrammiert. Das hier ist keineswegs die vollendete Seite,
-        sondern nur ein Prototyp, bei dem man sich einen (noch provisorischen)
-        Stundenplan generieren lassen kann. Wenn etwas nicht funktioniert, gibt
-        es auch keine Gewähr von uns, also wenn jemand zu spät zur Schule kommt,
-        weil der Stundenplan inkorrekt ist, übernehmen Timur und ich nicht die
-        Verantwortung dafür (ich hoffe, das ist nicht der Fall, aber Bugs gibt
-        es nun mal immer). Zu guter letzt, die Webseite hier ist nicht mit der
-        Schulleitung o.ä. abgeklärt, das ist einfach nur ein Hobby-Projekt von
-        uns beiden.
+        Das hier ist die inoffizielle MGM-Webseite von Max und Timur, gerippt von Maxl
       </p>
     </>
   )
